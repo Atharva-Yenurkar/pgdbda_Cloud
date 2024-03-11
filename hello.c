766dce34722793552fcaa7a9ea98fee35c97c601
chandan <you@example.com>
2024-03-11 12:48:28 +0530

    chandan

--- hello.c
+++ hello.c
@@ -4,12 +4,8 @@ int main()
   printf("Hello world");
   printf("Hii");
     printf("Hii");
-<<<<<<< HEAD
-     printf("Hi chandan here");
-=======
      printf("Hii");
-     printf("hello world");
->>>>>>> 63010f0818d9698a994a69c1ddcb424c7aec3654
+     printf("hello world by ");
 
   return 0;
 }
