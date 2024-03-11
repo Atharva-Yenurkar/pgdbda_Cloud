c7597f421cfbba5784c29eb0ce50f88e866a90e6
chandan <you@example.com>
2024-03-11 12:34:40 +0530

    chandan

--- hello.js
+++ hello.js
@@ -4,10 +4,7 @@ count++;
 if(count<=45)
 {
   console.log("Hello World");
-<<<<<<< HEAD
   console.log("avi changes");
-=======
   console.log("chandan Changes")
 
->>>>>>> f1b07d6e35fe54e5f72fa476af387628e6ed2dbb
 }
